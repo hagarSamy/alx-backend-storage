bc1e80e99046ba23f6e88e6a2bf006a229f2d4e7
hagarSamy <hs1800@fayoum.edu.eg>
2024-08-08 14:21:12 +0300

    users table

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,5 +1,7 @@
+-- SQL script that creates a table users
 CREATE TABLE IF NOT EXISTS users (
-    id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    id INT AUTO_INCREMENT NOT NULL,
     email VARCHAR(255) UNIQUE NOT NULL,
-    name VARCHAR(255)
+    name VARCHAR(255),
+    PRIMARY KEY (id)
 );
