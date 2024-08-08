76942349700879ee051156a679a9ad13757390dd
hagarSamy <hs1800@fayoum.edu.eg>
2024-08-08 15:56:16 +0300

    Old school band

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -4,5 +4,5 @@
 -- COALESCE(split, 2022) - formed AS lifespan
 SELECT band_name, IFNULL(split, 2022) - formed AS lifespan
 FROM metal_bands
-WHERE FIND_IN_SET ('Glam rock', style)
+WHERE FIND_IN_SET('Glam rock', style)
 ORDER BY lifespan DESC;
