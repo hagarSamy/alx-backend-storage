079b48f8cd1a2abb0f23d0afe540dd3048099116
hagarSamy <hs1800@fayoum.edu.eg>
2024-08-08 15:21:05 +0300

    best band

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,6 +1,6 @@
 -- a SQL script that ranks country origins of bands, ordered by the number of (non-unique) fans
 -- Column names must be: origin and nb_fans
-SELECT origin, COUNT(*) AS nb_fans
+SELECT origin,  SUM(fans) AS nb_fans
 from metal_bands
 GROUP BY Origin
 ORDER BY nb_fans DESC;
