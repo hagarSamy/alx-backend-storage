4855a214e3fc88d6b242b2d6583c3583d4c6d974
hagarSamy <hs1800@fayoum.edu.eg>
2024-08-14 01:32:22 +0300

    listing all from python

---
+++ 0x01-NoSQL/8-all.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""
+a Python function that lists all documents in a collection
+"""
+
+def list_all(mongo_collection):
+    '''lists all documents in a collection'''
+    docs = list(mongo_collection.find())
+    return docs
