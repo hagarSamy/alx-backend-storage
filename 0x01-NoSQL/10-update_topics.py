3e2325c8ab2eb8eb449185de8358ccbbf58ab89d
hagarSamy <hs1800@fayoum.edu.eg>
2024-08-14 02:58:36 +0300

    updaing topics

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -6,5 +6,12 @@ all topics of a school document based on the name
 
 
 def update_topics(mongo_collection, name, topics):
-    '''func body'''
-    mongo_collection.update_one({"name": name}, { "$set": {"topics": topics}})
+    '''Update the topics of a school document where the name matches.
+
+    :param mongo_collection: The MongoDB collection to update. pymongo 
+    :param name: The name of the school document to update.
+    :param topics: The list of topics to set in the document'''
+    mongo_collection.update_one(
+        {"name": name},
+        { "$set": {"topics": topics}}
+    )
