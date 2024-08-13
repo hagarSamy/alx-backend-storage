c34114aac90984dc8ad5c0c74b5434eb536ed1d1
hagarSamy <hs1800@fayoum.edu.eg>
2024-08-14 01:59:04 +0300

    insering from python and utilzing InsertOneResult object

---
+++ 0x01-NoSQL/9-insert_school.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+a Python function that
+inserts a new document in a collection based on kwargs
+"""
+
+
+def insert_school(mongo_collection, **kwargs):
+    '''func body'''
+    new_doc = mongo_collection.insert_one(kwargs)
+    return new_doc.inserted_id
