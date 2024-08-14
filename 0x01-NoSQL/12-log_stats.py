e26988b2def69656a53bec89059515e12fac6712
hagarSamy <hs1800@fayoum.edu.eg>
2024-08-14 03:57:58 +0300

    nginx stats

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -7,23 +7,23 @@ if __name__ == "__main__":
     client = MongoClient('mongodb://127.0.0.1:27017')
     nginx_collection = client.logs.nginx
     num_of_docs = nginx_collection.count_documents({})
-    print(f'{num_of_docs} logs\n')
-    print('Methods:\n')
+    print(f'{num_of_docs} logs')
+    print('Methods:')
 
     get_logs = nginx_collection.count_documents({"method": "GET"})
-    print(f'\t method GET: {get_logs}\n')
+    print(f'\t method GET: {get_logs}')
 
     POST_logs = nginx_collection.count_documents({"method": "POST"})
-    print(f'\t method POST: {POST_logs}\n')
+    print(f'\t method POST: {POST_logs}')
 
     PUT_logs = nginx_collection.count_documents({"method": "PUT"})
-    print(f'\t method PUT: {PUT_logs}\n')
+    print(f'\t method PUT: {PUT_logs}')
 
     PATCH_logs = nginx_collection.count_documents({"method": "PATCH"})
-    print(f'\t method PATCH: {PATCH_logs}\n')
+    print(f'\t method PATCH: {PATCH_logs}')
 
     DELETE_logs = nginx_collection.count_documents({"method": "DELETE"})
-    print(f'\t method DELETE: {DELETE_logs}\n')
+    print(f'\t method DELETE: {DELETE_logs}')
 
     status_checks = nginx_collection.count_documents({"method": "GET", "path": "/status"})
     print(f'{status_checks} status check')
