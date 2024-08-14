2511d2d104730b32c123b333a37014f9776d0969
hagarSamy <hs1800@fayoum.edu.eg>
2024-08-15 02:54:12 +0300

    more decoration for rpush

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -22,6 +22,20 @@ def count_calls(method: Callable) -> Callable:
         return method(self, *args, **kwargs)
     return wrapper
 
+def call_history(method: Callable) -> Callable:
+    '''a decorator to store the history of inputs
+    and outputs for a particular function.'''
+    qualname = method.__qualname__
+    input_key = qualname + ":inputs"
+    output_key = qualname + ":outputs"
+
+    @functools.wraps(method)
+    def wrapper(self, *args, **kwargs):
+        self._redis.rpush(input_key, str(args))
+        output = method(self, *args, **kwargs)
+        self._redis.rpush(output_key, str(output))
+        return output
+    return wrapper
 
 class Cache:
     '''store an instance of the Redis client'''
@@ -31,6 +45,7 @@ class Cache:
         self._redis.flushdb()
 
     @count_calls
+    @call_history
     def store(self, data: Union[str, bytes, int, float]) -> str:
         '''generate a random key
         store the input data in Redis
