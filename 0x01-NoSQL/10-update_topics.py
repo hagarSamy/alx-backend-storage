e83ecaf7abb9aebeaba411b999223e64360d0681
hagarSamy <hs1800@fayoum.edu.eg>
2024-08-14 03:00:00 +0300

    updaing topics

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -13,6 +13,5 @@ def update_topics(mongo_collection, name, topics):
     :param topics: The list of topics to set in the document'''
     mongo_collection.update_one(
         {"name": name},
-        { "$set": {"topics": topics}},
-        {"multi": "true"}
+        { "$set": {"topics": topics}}
     )
